bce6c171bdd8c8fb52cee1aa168b4062d8831521
mwlon <m.w.loncaric@gmail.com>
2022-03-14 18:56:25 -0400

    adjust default segment sizes

--- src/opt.rs
+++ src/opt.rs
@@ -27,10 +27,10 @@ pub struct Opt {
 
   // Segments should complete shortly after reaching either the target
   // number of rows or the target uncompressed size (whichever comes first).
-  #[structopt(long, default_value = "10000000")]
+  #[structopt(long, default_value = "5000000")]
   pub target_rows_per_segment: u32,
 
-  #[structopt(long, default_value = "130000000")] // just under 128MB
+  #[structopt(long, default_value = "260000000")] // just under 256MB
   pub target_uncompressed_bytes_per_segment: u64,
 
   // the fewest number of rows in a segment before compaction
