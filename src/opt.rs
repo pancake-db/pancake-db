e13545864dcbdfd705253c7c0518cd19d140bee3
mwlon <m.w.loncaric@gmail.com>
2022-03-14 13:48:28 -0400

    tweak default

--- src/opt.rs
+++ src/opt.rs
@@ -25,12 +25,12 @@ pub struct Opt {
   #[structopt(flatten)]
   pub cloud_opts: CloudOpt,
 
-  // a target number of rows for each segment of data
-  // Segments should complete slightly after this row count
+  // Segments should complete shortly after reaching either the target
+  // number of rows or the target uncompressed size (whichever comes first).
   #[structopt(long, default_value = "10000000")]
   pub target_rows_per_segment: u32,
 
-  #[structopt(long, default_value = "134217728")]
+  #[structopt(long, default_value = "130000000")] // just under 128MB
   pub target_uncompressed_bytes_per_segment: u64,
 
   // the fewest number of rows in a segment before compaction
